64bca095de1432f91dd2976313e4903c699f321d
Ilyas Amezouar <ilyasa@uniform.dev>
2024-01-29 13:23:14 +0100

    add app-root

---
+++ apps/app-root/pages/[[...slug]].tsx
@@ -0,0 +1,26 @@
+import { RootComponentInstance } from "@uniformdev/canvas";
+import { withUniformGetServerSideProps } from "@uniformdev/canvas-next/route";
+import { UniformComposition, UniformSlot } from "@uniformdev/canvas-react";
+import "../components/canvasComponents";
+import Head from "next/head";
+
+export const getServerSideProps = withUniformGetServerSideProps({});
+
+export default function Page({
+  data: composition,
+}: {
+  data: RootComponentInstance;
+}) {
+  return (
+    <>
+      <Head>
+        <title>{composition._name}</title>
+      </Head>
+      <main className="text-orange">
+        <UniformComposition data={composition}>
+          <UniformSlot name="content" />
+        </UniformComposition>
+      </main>
+    </>
+  );
+}
